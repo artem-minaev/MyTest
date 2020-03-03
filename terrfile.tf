1409a6abea2fca4726cd35aa80a60cf99f7f32af
artem-minaev <58518057+artem-minaev@users.noreply.github.com>
2020-03-03 23:58:05 +0200

    Update terrfile.tf

--- terrfile.tf
+++ terrfile.tf
@@ -3,8 +3,7 @@
 
 provider "aws" {
     region = "us-east-2"
-    access_key = "AKIAYX7LCQ24HTX75DES"
-    secret_key = "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"
+   
 }
 
 resource "aws_instance" "web_server_test" {
