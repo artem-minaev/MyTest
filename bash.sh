9b3529919fdd0284fe17f5c59e50edf537aa1ab2
artem-minaev <58518057+artem-minaev@users.noreply.github.com>
2020-03-07 21:22:21 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 apt -y update
 apt -y install apache2
+myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
+echo "<h1>Web Server with ip: $myip</h1><br>Build by Terraform!<br>Use bash to have more" > /var/www/html/index.html
