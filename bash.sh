0676217a09f1ec0fc8b1c0568b65b90f8bae4de3
artem-minaev <58518057+artem-minaev@users.noreply.github.com>
2020-03-07 21:01:03 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,5 @@
-apt -y update
-apt -y install httpd
+sudo apt -y update
+sudo apt -y install apache2
 myip=`curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h1>Web Server with ip: $myip</h1><br>Build by Terraform!<br>Use bash to have more" > /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
