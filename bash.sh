2d28ae5ff332103fab20a163eb22b27c216f8838
artem-minaev <58518057+artem-minaev@users.noreply.github.com>
2020-03-08 17:51:24 +0200

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -5,4 +5,5 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt-get -y update
 apt-get -y install docker-ce docker-ce-cli containerd.io
-
+sudo docker pull artmin/minaev-doc
+sudo docker run -d -p 80:80 artmin/minaev-doc
