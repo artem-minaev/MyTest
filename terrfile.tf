113d2e140ca498bd75b0645897f408f8cbc1aae5
artem-minaev <58518057+artem-minaev@users.noreply.github.com>
2020-03-12 22:46:32 +0200

    Update terrfile.tf

--- terrfile.tf
+++ terrfile.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "web_server_test" {
   instance_type = "t2.micro"
   key_name = "MyKeyPair"
   vpc_security_group_ids = [aws_security_group.web_server_test.id]
-  user_data = file("script.sh")
+  user_data = file("bash.sh")
 
   tags =  {
     Name = "Ubuntu Server 18.04"
