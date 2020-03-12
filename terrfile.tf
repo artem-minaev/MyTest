23164c86801075efb46ff08e4b721a093c8d9fd4
artem-minaev <58518057+artem-minaev@users.noreply.github.com>
2020-03-12 23:08:16 +0200

    Update terrfile.tf

--- terrfile.tf
+++ terrfile.tf
@@ -1,19 +1,18 @@
 provider "aws" {
     region = "us-east-2"
-   
 }
 
 resource "aws_instance" "web_server_test" {
-  ami = "ami-0fc20dd1da406780b" # aws
+  ami = "ami-0fc20dd1da406780b" # ubuntu18
   instance_type = "t2.micro"
   key_name = "MyKeyPair"
   vpc_security_group_ids = [aws_security_group.web_server_test.id]
   user_data = file("bash.sh")
 
   tags =  {
-    Name = "Ubuntu Server 18.04"
-    Owner = "chebotarev"
-    Project = "Epam_test_project"
+    Name = "AWS server"
+    Owner = "Artem Minaev"
+    Project = "Terraform deploy"
   }
 }
 
