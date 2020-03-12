a7cb071f240e0afd193d259fd660e1f9e1c3ad27
artem-minaev <58518057+artem-minaev@users.noreply.github.com>
2020-03-12 22:45:21 +0200

    Update terrfile.tf

--- terrfile.tf
+++ terrfile.tf
@@ -1,31 +1,29 @@
-# Terraform
-# Build web sever
-
 provider "aws" {
-    region = "us-east-2"
+    region = "us-east-1"
+   
 }
 
 resource "aws_instance" "web_server_test" {
-  ami = "ami-0959e8feedaf156bf"
+  ami = "ami-07ebfd5b3428b6f4d" # aws
   instance_type = "t2.micro"
   key_name = "MyKeyPair"
   vpc_security_group_ids = [aws_security_group.web_server_test.id]
-  user_data = file("bash.sh")
+  user_data = file("script.sh")
 
   tags =  {
-    Name = "AWS server"
-    Owner = "Artem Minaev"
-    Project = "Terraform deploy"
+    Name = "Ubuntu Server 18.04"
+    Owner = "chebotarev"
+    Project = "Epam_test_project"
   }
 }
 
 resource "aws_security_group" "web_server_test" {
   name        = "web_server_security_group"
-  description = "Allow 80,8080,22 tcp"
+  description = "Allow 80,433,8080,22 tcp"
 
 
   dynamic "ingress"{
-    for_each = ["80","22","8080"]
+    for_each = ["80","22","8080","443"]
     content{
       from_port   = ingress.value
       to_port     = ingress.value
